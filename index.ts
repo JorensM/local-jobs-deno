83d4bb193c258049bf65ce062f58dd7961f8cd6f
JorensM <jorensmerenjanu@gmail.com>
2023-09-08 09:12:05 +0300

    test endpoint

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
+import { serve } from 'https://deno.land/std@0.201.0/http/server.ts'
+
+const port = 8080
+
 console.log('hello from deno!')
+
+const handler = (request: Request): Response => {
+    console.log(request)
+    return new Response("hello from deno!", { status : 200 })
+}
+
+await serve(handler, { port })
