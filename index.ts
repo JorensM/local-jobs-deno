b270af8f7b03adfac7ab38a4d29ae9b48be578b1
JorensM <jorensmerenjanu@gmail.com>
2023-09-08 12:08:13 +0300

    Revert "debugging"
    This reverts commit 6c55a148ba2c663528e71e0e673b5faee41483ce.

--- index.ts
+++ index.ts
@@ -55,8 +55,6 @@ function addContactToUser(
             throw new Error('Missing env vars in addContactToUser()')
         }
         console.log('4')
-        console.log(user_id)
-        console.log(contact_id)
         const promise = db.listDocuments<UserDocument>(
             DB_ID,
             COLLECTION_USERS_ID,
