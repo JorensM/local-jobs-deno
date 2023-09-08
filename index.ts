04fabf9de336c43a2f16c578d59e983c4000caa6
JorensM <jorensmerenjanu@gmail.com>
2023-09-08 09:15:19 +0300

    testing

--- index.ts
+++ index.ts
@@ -1,12 +1,8 @@
-import { serve } from 'https://deno.land/std@0.201.0/http/server.ts'
-
-const port = 8080
-
 console.log('hello from deno!')
 
 const handler = (request: Request): Response => {
     console.log(request)
-    return new Response("hello from deno!", { status : 200 })
+    return new Response("hello from deno new!", { status : 200 })
 }
 
-await serve(handler, { port })
+await Deno.serve(handler)
