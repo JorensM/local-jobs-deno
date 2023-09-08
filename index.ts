e52d4a2b84bbb312c87fd5d2caeeec9fef999671
JorensM <jorensmerenjanu@gmail.com>
2023-09-08 09:20:51 +0300

    set up endpoint

--- index.ts
+++ index.ts
@@ -1,8 +1,25 @@
-console.log('hello from deno!')
+const endpoint_url = {
+    stripe_contact_payment_success: 'api/stripe_contact_payment_success'
+}
 
 const handler = (request: Request): Response => {
-    console.log(request)
-    return new Response("hello from deno new!", { status : 200 })
+
+    const pathname = new URL(request.url).pathname
+
+    console.log(pathname)
+
+    switch(pathname) {
+        case endpoint_url.stripe_contact_payment_success: {
+            return new Response('payment success!')
+            //break
+        }
+        default: {
+            return new Response('404 Not found', { status: 404 })
+        }
+    }
+
+    //console.log(request)
+    //return new Response("hello from deno new!", { status : 200 })
 }
 
 Deno.serve(handler)
