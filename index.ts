08b90a453e9c37dbf5eba6c6485edff707216ce5
JorensM <jorensmerenjanu@gmail.com>
2023-09-08 09:34:44 +0300

    added env variable validation and configure appwrite client

--- index.ts
+++ index.ts
@@ -1,16 +1,38 @@
+import { Client, Databases } from 'npm:node-appwrite'
+
 const endpoint_url = {
     stripe_contact_payment_success: '/api/stripe-contact-payment-success'
 }
 
-const handler = (request: Request): Response => {
+if (
+    !Deno.env.has('APPWRITE_ENDPOINT') || 
+    !Deno.env.has('APPWRITE_API_KEY') ||
+    !Deno.env.has('APPWRITE_PROJECT_ID')
+) {
+    throw new Error('Required ENV variables are not set')
+}
 
-    const pathname = new URL(request.url).pathname
+function addContactToUser(contact_id: string, user_id: string) {
 
-    console.log(pathname)
+}
+
+const handler = (req: Request): Response => {
+
+    const client = new Client()
+
+    client
+        .setEndpoint(Deno.env.get('APPWRITE_ENDPOINT')!)
+        .setKey(Deno.env.get('APPWRITE_API_KEY')!)
+        .setProject(Deno.env.get('APPWRITE_PROJECT_ID')!)
+
+    const pathname = new URL(req.url).pathname
+
+    console.log('HTTP request at ' + pathname)
 
     switch(pathname) {
         case endpoint_url.stripe_contact_payment_success: {
-            return new Response('payment success!')
+            //addContactToUser()
+            return new Response(req.body)
             //break
         }
         default: {
